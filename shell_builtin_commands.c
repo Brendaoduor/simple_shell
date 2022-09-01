908cd9cfd693ca2f1dc3b063f94144900ac03335
brenda oduor <bresharchie2@gmail.com>
2022-09-02 00:15:16 +0300

    changes made

--- shell_builtin_commands.c
+++ shell_builtin_commands.c
@@ -6,29 +6,33 @@ int shell_cd(char **args)
 {
 	if (args[1] == NULL)
 	{
-		perror("cd: no such argument\n");
+		perror("cd: missing argument\n");
 	}
 	else
 	{
 		if (chdir(args[1] != 0))
-		perror("cd");
+			perror("cd\n");
 	}
 	return (1);
 }
 
 int shell_help(char **args)
 {
-char *helptext = 
-	"the following are the only commands available\n"
-	"cd	change the working directory\n"
-	"exit	exits the shell.\n"
-	"help	print this help text\n";
-printf("%s", helptext);
+	if (args[1] != NULL)
+	{
+	char *helptext =
+		"the following are the only commands available\n"
+		"cd	change the working directory\n"
+		"exit	exits the shell.\n"
+		"help	print this help text\n";
+	printf("%s", helptext);
+	}
 
 return (1);
 }
 
 int shell_exit(char **args)
 {
-return (0);
+	if (args[1] == NULL)
+	return (0);
 }
