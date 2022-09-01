a61c6b14d0e9dedc9f7f81baa5eed4899131cb45
brenda oduor <bresharchie2@gmail.com>
2022-09-01 23:15:16 +0300

    changes made

--- shell_tokenization_funcs.c
+++ shell_tokenization_funcs.c
@@ -49,28 +49,4 @@ char **tokenize_line(char *buffer, char *delim, int token_no)
 	
 	return (argv);
 }
-char *_strncpy(char *dest, char *src)
-{
-	char *copy = dest;
-
-	while (*src)
-	{
-		*dest++ = *src++;
-		return (copy);
-	}
-}
-
-int _strcmp(char *s1, char *s2)
-{
-	while ((s1 && s2) != NULL)
-	{
-		if (s1 != s2)
-		{
-			return (*s1 - *s2)
-		}
-	s1++;
-	s2++;
-	}
-	return (*s1 - *s2);
-}
 
